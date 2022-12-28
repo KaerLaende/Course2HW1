64278bad5b3380b4e69ec5f9c58e60fcd776e254
Kaerlaende <kaerlaende@mail.ru>
2022-12-28 14:31:22 +0300

    Часть 2. Инкапсуляция Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 import Transport.Car;
 
+import java.security.Key;
+
 public class Main {
     public static void main(String[] args) {
-    Car bmw = new Car("робот","купе","",5,true);
+    Car bmw = new Car("BMW", "z8",4.4,"синий",2022,"Германия","робот","седан","o007oo123",4,true);
     bmw.setBodyType("седан");
         System.out.println(bmw.isTireType());
     bmw.changeTireType();
         System.out.println(bmw);
+        Car.Key bmwKey = bmw.new Key(true, true);
+        System.out.println(bmw+" "+bmwKey);
     }
 }
--- src/Transport/Car.java
+++ src/Transport/Car.java
@@ -3,21 +3,21 @@ package Transport;
 public class Car {
 
     //"имеют модификатор final":
-    final String brand ="BMW";
-    final String model= "M5";
-    final double engineVolume= 4.4;
-    final String color="синий";
-    final int year=2022;
-    final String country="Германия";
+    private final String brand;
+    private final String model;
+    private final double engineVolume;
+    private final String color;
+    private final int year;
+    private final String country;
 
 
     /*       Добавляем:
-                    «Коробка передач»,
-                    «Тип кузова»,
-                    «Регистрационный номер»,
-                    «Количество мест»,
-                    признак «Летняя» или «Зимняя резина».
-            */
+                        «Коробка передач»,
+                        «Тип кузова»,
+                        «Регистрационный номер»,
+                        «Количество мест»,
+                        признак «Летняя» или «Зимняя резина».
+                */
     private String transmission;
     private String bodyType;
     private String regNumber;
@@ -55,7 +55,10 @@ public class Car {
         return bodyType;
     }
     public void setBodyType(String bodyType) {
-        this.bodyType = bodyType;
+        if (bodyType != null && !bodyType.isEmpty() && !bodyType.isBlank()) {
+            this.bodyType = bodyType;
+        } else {
+        this.bodyType = bodyType;}
     }
     public String getRegNumber() {
         return regNumber;
@@ -79,7 +82,36 @@ public class Car {
         this.tireType = tireType;
     }
 
-    public Car(String transmission, String bodyType, String regNumber, int seatsNumber, boolean tireType) {
+    public Car(String brand, String model, double engineVolume, String color, int year, String country, String transmission, String bodyType, String regNumber, int seatsNumber, boolean tireType) {
+        if (brand != null && !brand.isEmpty() && !brand.isBlank()) {
+            this.brand = brand;
+        } else {
+            this.brand = "brand";
+        }
+        if (model != null && !model.isEmpty() && !model.isBlank()) {
+            this.model = model;
+        } else {
+            this.model = "model";
+        }
+        if(engineVolume<=0){
+            this.engineVolume=1.6;
+        }else {
+        this.engineVolume = engineVolume;}
+
+        if (color!= null && !color.isEmpty() && !color.isBlank()) {
+            this.color = color;
+        } else {
+            this.color = "color";
+        }
+        if (year<1950){
+            this.year=2000;
+        }else {
+        this.year = year;}
+        if (country!= null && !country.isEmpty() && !country.isBlank()) {
+            this.country = country;
+        } else {
+            this.country = "country";
+        }
         if (transmission != null && !transmission.isEmpty() && !transmission.isBlank()) {
             this.transmission = transmission;
         } else {
@@ -92,19 +124,58 @@ public class Car {
         }
         if (regNumber != null && !regNumber.isEmpty() && !regNumber.isBlank()) {
             this.regNumber = regNumber;
+        } else if (!sayCorrectRegNumber()) {
+            this.regNumber="не корректный номер";
         } else {
             this.regNumber = "без номера";
         }
-        this.seatsNumber = seatsNumber;
+        if(seatsNumber<1){
+            this.seatsNumber=2;
+        }else {
+            this.seatsNumber = seatsNumber;
+        }
         this.tireType = tireType;
     }
 
 
+
     @Override
     public String toString() {
-        return brand+" "+ model+" "+year+" год выпуска, сборка в стране "+country+", "+color+", объем двигателя — "+engineVolume+ " л."+" Коробка передач: "+transmission+"; Тип кузова: "+bodyType+"; Регистрационный номер: "+regNumber+"; Количество мест: "+seatsNumber+"; Резина: "+(tireType?"летняя.":"зимняя.");
+        return brand+" "+ model+" "+year+" год выпуска, сборка в стране "+country+", "+color+", объем двигателя — "+engineVolume+ " л."+
+                " Коробка передач: "+transmission+"; Тип кузова: "+
+                bodyType+"; Регистрационный номер: "+regNumber+"; Количество мест: "+seatsNumber+"; Резина: "+(tireType?"летняя.":"зимняя.");
     }
+    //проверка правильности заполнения регистрационного номера "a000bc000":
+    public boolean sayCorrectRegNumber(){
+        if(regNumber.length()!=9){
+            return false;
+        }
+        char[] chars=regNumber.toCharArray();
+            if(!Character.isAlphabetic(chars[0])||!Character.isAlphabetic(chars[4])||!Character.isAlphabetic(chars[5])){
+                return false;
+            }
+            if (!Character.isDigit(chars[1])||!Character.isDigit(chars[2])||!Character.isDigit(chars[3])||!Character.isDigit(chars[6])||!Character.isDigit(chars[7])||!Character.isDigit(chars[8])){
+                return false;
+            }
+            return true;
+    }
+
     public boolean changeTireType(){
         return tireType= !tireType;
     }
+      public class Key{
+       private final boolean outStart;
+       private final boolean noKeyAccess;
+
+        @Override
+        public String toString() {
+            return (outStart?"Удаленный запуск двигателя;": "")+ (noKeyAccess?" Бесключевой доступ":"");
+            }
+
+        public Key(boolean outStart, boolean noKeyAccess) {
+            this.outStart = outStart;
+            this.noKeyAccess = noKeyAccess;
+        }
+    }
+
 }
